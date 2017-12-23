993eca4bc2a0135ef431394549372d7af8f97d5a
juan-herrador <juanitok1984@gmail.com>
2017-12-24 00:55:17 +0100

    Create Controller-Service communication

---
+++ src/main/java/com/merapar/assessment/service/XmlProcessorService.java
@@ -0,0 +1,9 @@
+package com.merapar.assessment.service;
+
+import com.merapar.assessment.model.Input;
+import com.merapar.assessment.model.Output;
+
+public interface XmlProcessorService {
+
+    Output process(Input input);
+}
