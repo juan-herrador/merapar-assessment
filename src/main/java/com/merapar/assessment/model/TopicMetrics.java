d7fd9a76f99734c9b60a69f361e1861627352f6b
juan-herrador <juanitok1984@gmail.com>
2017-12-24 17:37:14 +0100

    Add test to GitHubTopicXmlStreamReaderService

--- src/main/java/com/merapar/assessment/model/TopicMetrics.java
+++ src/main/java/com/merapar/assessment/model/TopicMetrics.java
@@ -1,6 +1,7 @@
 package com.merapar.assessment.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class TopicMetrics {
 
@@ -19,6 +20,20 @@ public class TopicMetrics {
     private int totalAnswerCount = 0;
 
     private int totalCommentCount = 0;
+
+    public TopicMetrics() {
+    }
+
+    public TopicMetrics(LocalDateTime firstPost, LocalDateTime lastPost, int totalPosts, int totalAcceptedPosts, int totalScore, int totalViewCount, int totalAnswerCount, int totalCommentCount) {
+        this.firstPost = firstPost;
+        this.lastPost = lastPost;
+        this.totalPosts = totalPosts;
+        this.totalAcceptedPosts = totalAcceptedPosts;
+        this.totalScore = totalScore;
+        this.totalViewCount = totalViewCount;
+        this.totalAnswerCount = totalAnswerCount;
+        this.totalCommentCount = totalCommentCount;
+    }
 
     public void addPostData(PostData postData) {
         if (this.firstPost == null || this.firstPost.isAfter(postData.getCreationDate())) {
@@ -67,5 +82,26 @@ public class TopicMetrics {
 
     public int getTotalCommentCount() {
         return totalCommentCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TopicMetrics that = (TopicMetrics) o;
+        return totalPosts == that.totalPosts &&
+                totalAcceptedPosts == that.totalAcceptedPosts &&
+                totalScore == that.totalScore &&
+                totalViewCount == that.totalViewCount &&
+                totalAnswerCount == that.totalAnswerCount &&
+                totalCommentCount == that.totalCommentCount &&
+                Objects.equals(firstPost, that.firstPost) &&
+                Objects.equals(lastPost, that.lastPost);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(firstPost, lastPost, totalPosts, totalAcceptedPosts, totalScore, totalViewCount, totalAnswerCount, totalCommentCount);
     }
 }
